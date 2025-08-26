78483425e8014f4d7137bebca5268a29e733206f
bforcum <bmforcum@outlook.com>
2025-08-26 14:27:33 -0700

    Add scroll view

--- lib/page/results_page.dart
+++ lib/page/results_page.dart
@@ -23,35 +23,38 @@ class _ResultsPageState extends ConsumerState<ResultsPage> {
   @override
   Widget build(BuildContext context) {
     matchResults = ref.watch(storedMatchResultsProvider);
-    return Padding(
-      padding: EdgeInsets.all(20),
-      child: Column(
-        spacing: 20,
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              hintText: "Filter team number",
-              hintStyle: TextStyle(
-                fontSize: 20,
-                color: Theme.of(context).hintColor,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          spacing: 20,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Filter team number",
+                hintStyle: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).hintColor,
+                ),
               ),
             ),
-          ),
-          Column(
-            spacing: 10,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: matchResults.when(
-              loading: () => [CircularProgressIndicator()],
-              error: (error, stack) => [Text("Something went wrong: \n$error")],
-              data: (results) {
-                if (results.isEmpty) return [Text("No results yet")];
-                return results
-                    .map((e) => MatchResultCard(matchResult: e))
-                    .toList();
-              },
+            Column(
+              spacing: 10,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: matchResults.when(
+                loading: () => [CircularProgressIndicator()],
+                error:
+                    (error, stack) => [Text("Something went wrong: \n$error")],
+                data: (results) {
+                  if (results.isEmpty) return [Text("No results yet")];
+                  return results
+                      .map((e) => MatchResultCard(matchResult: e))
+                      .toList();
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
