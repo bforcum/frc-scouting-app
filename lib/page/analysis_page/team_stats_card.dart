38830119ae33893565f85c388d37ce4154ae29b1
bforcum <113265535+bforcum@users.noreply.github.com>
2025-12-13 15:23:15 -0800

    Make team stats cards expand on tap

--- lib/page/analysis_page/team_stats_card.dart
+++ lib/page/analysis_page/team_stats_card.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:scouting_app/consts.dart';
 import 'package:scouting_app/model/team_data.dart';
 
-class TeamStatsCard extends ConsumerWidget {
+class TeamStatsCard extends ConsumerStatefulWidget {
   final TeamData data;
   final int Function(TeamData) analysisFunc;
 
@@ -14,54 +14,91 @@ class TeamStatsCard extends ConsumerWidget {
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<TeamStatsCard> createState() => _TeamStatsCardState();
+}
+
+class _TeamStatsCardState extends ConsumerState<TeamStatsCard> {
+  bool isExpanded = false;
+  @override
+  Widget build(BuildContext context) {
     Color accent =
-        (data.totalScores.length) < 12 ? Color(0xFFFFCC00) : Color(0xFF009900);
-    return GestureDetector(
-      // onTap: () async => await _viewResults(context),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.surfaceContainer,
-          borderRadius: BorderRadius.circular(kBorderRadius),
-        ),
-        padding: const EdgeInsets.all(kBorderRadius),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Team ${data.teamNumber}",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  "Match count: ${data.totalScores.length}",
-                  style: TextStyle(fontSize: 16),
-                ),
-              ],
-            ),
-            const Spacer(),
-            Container(
-              margin: EdgeInsets.all(5),
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              alignment: Alignment.center,
-              height: 60,
+        (widget.data.totalScores.length) < 12
+            ? Color(0xFFFFCC00)
+            : Color(0xFF009900);
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surfaceContainer,
+        borderRadius: BorderRadius.circular(kBorderRadius),
+      ),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap:
+                () => setState(() {
+                  isExpanded = !isExpanded;
+                }),
+
+            child: Container(
+              padding: const EdgeInsets.all(kBorderRadius),
               decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.surfaceContainer,
                 borderRadius: BorderRadius.circular(kBorderRadius),
-                border: Border.all(color: accent),
-                color: accent.withAlpha(32),
               ),
               child: Row(
-                spacing: 10,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Icon(Icons.analytics, color: accent),
-                  Text("${analysisFunc(data)}", style: TextStyle(fontSize: 20)),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Team ${widget.data.teamNumber}",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "Match count: ${widget.data.totalScores.length}",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    alignment: Alignment.center,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(kBorderRadius),
+                      border: Border.all(color: accent),
+                      color: accent.withAlpha(32),
+                    ),
+                    child: Row(
+                      spacing: 10,
+                      children: [
+                        Icon(Icons.analytics, color: accent),
+                        Text(
+                          "${widget.analysisFunc(widget.data)}",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+          Visibility(
+            visible: isExpanded,
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Row(children: [
+                
+              ],),
+            ),
+          ),
+        ],
       ),
     );
   }
